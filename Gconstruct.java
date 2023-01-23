9e262345c7aab280946a050fd6efe25ececa0e58
Ashin-paul <111363393+Ashin-paul@users.noreply.github.com>
2023-01-23 04:50:50 +0000

    ok

--- Gconstruct.java
+++ Gconstruct.java
@@ -1,7 +1,7 @@
-class Gen <T,U> {
+class Gen <T,U> { //generic class
     T v1;
     U v2;
-    Gen(T o1,U o2){
+    Gen(T o1,U o2){ //generic constructor
         v1=o1;
         v2=o2;
         System.out.println(v1+" --- "+v2);
@@ -9,7 +9,7 @@ class Gen <T,U> {
 }
 class Gconstruct{
     public static void main(String args[]){
-        Gen <String,Integer> g1 =new Gen("A",123);
-        Gen <Double,String> g2 =new Gen(12.11,"B");
+        Gen <String,Integer> g1 =new Gen("A",1111);
+        Gen <Double,String> g2 =new Gen(11.11,"A");
     }
 }
